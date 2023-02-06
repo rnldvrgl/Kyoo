b7ce0ce676174026396d29bfe20211c1a58393c4
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-02-06 18:53:37 +0800

    initial commit to department-sect

--- pages/departments/main-admin/departments.php
+++ pages/departments/main-admin/departments.php
@@ -38,15 +38,33 @@
 		</div>
 		<section class="section">
 			<div class="row">
-				<div class="col-lg-6">
+				<div class="col-lg-8">
 					<div class="card">
 						<div class="card-body">
 							<h5 class="card-title">Example Card</h5>
 							<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel ex error magnam earum quia? Aut distinctio autem quis ullam magnam modi amet repellat, enim quasi esse, non totam ipsa quo.</p>
 						</div>
 					</div>
+					<!-- <table id="table_id" class="display">
+						<thead>
+							<tr>
+								<th>Column 1</th>
+								<th>Column 2</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Row 1 Data 1</td>
+								<td>Row 1 Data 2</td>
+							</tr>
+							<tr>
+								<td>Row 2 Data 1</td>
+								<td>Row 2 Data 2</td>
+							</tr>
+						</tbody>
+					</table> -->
 				</div>
-				<div class="col-lg-6">
+				<div class="col-lg-4">
 					<div class="card">
 						<div class="card-body">
 							<h5 class="card-title">Example Card</h5>
@@ -61,9 +79,12 @@
 
 	<!-- Bootstrap JS -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-</body>
 
-<!-- Form Validate -->
-<!-- <script src="../../assets/js/validateForm.js"></script> -->
+	<!-- DataTable JS -->
+	<script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
+
+	<!-- Main JS -->
+	<script src="../../../assets/js/main.js"></script>
+</body>
 
 </html>
--- pages/includes/footer.php
+++ pages/includes/footer.php
@@ -1,8 +1,10 @@
 	<!-- Bootstrap JS -->
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-	<script src="../../assets/js/chart.min.js"></script>
-	<script src="../../assets/js/validate.js"></script>
+	<!-- DataTable JS -->
+	<script src="https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js"></script>
+
+	<!-- Main JS -->
 	<script src="../../assets/js/main.js"></script>
 	</body>
 
