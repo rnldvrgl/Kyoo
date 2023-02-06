75df2a3aedc5ecc3d16d1549385f54f6edaf7f34
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-02-06 18:21:30 +0800

    edit main admin dashboard

--- assets/js/validateForm.js
+++
@@ -1,9 +0,0 @@
-const form = document.querySelector("form");
-
-form.addEventListener("submit", (e) => {
-    if (!form.checkValidity()) {
-        e.preventDefault();
-    }
-
-    form.classList.add("was-validated");
-});
