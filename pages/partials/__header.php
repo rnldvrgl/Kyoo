b86f362cdd9af41a44844267843ccf2ab8836892
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-02-11 12:25:12 +0800

    edit profile page

--- pages/partials/__header.php
+++ pages/partials/__header.php
@@ -2,7 +2,7 @@
 	<header id="header" class="header fixed-top d-flex align-items-center bg-kyoodark">
 		<div class="d-flex align-items-center justify-content-between">
 			<a href="#" class="logo d-flex align-items-center">
-				<img src="../../../assets/img/kyoo-logo.png" alt="" />
+				<img src="/../Kyoo/assets/img/kyoo-logo.png" alt="" />
 				<span class="d-none d-lg-block">Queueing Management System</span>
 			</a>
 			<i class="fa-solid fa-bars toggle-sidebar-btn"></i>
@@ -15,7 +15,7 @@
 				</li>
 				<li class=" nav-item dropdown pe-3">
 					<a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-						<img src="../../../assets/img/profile-img.jpg" alt="Profile" class="rounded-circle" />
+						<img src="/../Kyoo/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle" />
 						<span class="d-none d-md-block dropdown-toggle ps-2">Mark Liwins</span>
 					</a>
 					<ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
@@ -27,18 +27,9 @@
 							<hr class="dropdown-divider" />
 						</li>
 						<li>
-							<a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+							<a class="dropdown-item d-flex align-items-center" href="../user-profile.php">
 								<i class="fa-solid fa-user"></i>
 								<span>My Profile</span>
-							</a>
-						</li>
-						<li>
-							<hr class="dropdown-divider" />
-						</li>
-						<li>
-							<a class="dropdown-item d-flex align-items-center" href="users-profile.html">
-								<i class="fa-solid fa-gear"></i>
-								<span>Account Settings</span>
 							</a>
 						</li>
 						<li>
