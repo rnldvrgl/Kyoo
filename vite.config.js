287941307db113db5a6a67030753c9add2b16783
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-03-09 19:22:29 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-const path = require("path");
-
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
+import path from "path";
 
 export default defineConfig({
     plugins: [
