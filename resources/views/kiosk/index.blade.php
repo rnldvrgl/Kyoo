cdead7fdada40d94b70262ea6e4d57e218f555b3
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-03-09 11:42:25 +0800

    fix kiosk index page

--- resources/views/kiosk/index.blade.php
+++ resources/views/kiosk/index.blade.php
@@ -7,8 +7,9 @@
 
 <x-layout>
     <div class="opacity-25" id="background-image"></div>
-    <section class="container d-flex justify-content-center vh-100 p-5">
-        <div class="row row-cols-2 justify-content-center align-items-center h-100 w-100 p-lg-5 position-relative">
+    <section class="container d-flex justify-content-center align-items-center min-vh-100 p-5">
+        <div
+            class="row row-cols-1 row-cols-md-2 justify-content-center align-items-center h-100 w-100 p-lg-5 position-relative">
             @if (isset($message))
                 <div id="message"
                     class="alert alert-success position-absolute top-0 start-50 translate-middle text-center mt-5"
@@ -16,7 +17,8 @@
                     {{ $message }}
                 </div>
             @endif
-            <div class="col-9 bg-light gap-3 shadow d-flex flex-column justify-content-center align-items-center py-5">
+            <div
+                class="col-md-9 bg-light gap-3 shadow d-flex flex-column justify-content-center align-items-center py-5">
                 <h3 class="fw-semibold text-uppercase" data-aos="fade-down">
                     Welcome to
                 </h3>
@@ -25,7 +27,7 @@
                     Queueing Management System
                 </h4>
 
-                <div class="d-grid col-6 mx-auto p-3" data-aos="zoom-in" data-aos-easing="ease-in-out">
+                <div class="d-grid col-8 col-md-6 mx-auto p-3" data-aos="zoom-in" data-aos-easing="ease-in-out">
                     <a href="{{ route('select-department') }}" class="btn btn-kyoored text-uppercase">
                         Get Started
                     </a>
