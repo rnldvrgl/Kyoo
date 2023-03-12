cb69a0a113fe7b6141937332235bedb6d6e05ff5
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-03-12 16:10:34 +0800

    add error 429 page

---
+++ resources/views/errors/429.blade.php
@@ -0,0 +1,23 @@
+{{-- Page Title --}}
+@section('mytitle', '429')
+
+<x-layout>
+    <!-- Main Content -->
+    <main>
+        <div class="container">
+            <section class="section error-404">
+                <div class="row">
+                    <div class="col-md-7">
+                        <img src="{{ asset('assets/images/error-429.png') }}" class="img-fluid py-5" alt="Page Not Found">
+                    </div>
+                    <div class="col-md-5 d-flex flex-column justify-content-center align-items-right gap-3">
+                        <h1>Error 429</h1>
+                        <h2>Too Many Requests.</h2>
+                        <a class="btn rounded-pill btn-kyoored" href="javascript:history.go(-1)">Back to home</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+    <!-- /Main Content -->
+</x-layout>
