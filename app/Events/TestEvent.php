9103cdad3afff22c8065f705a60157be9caf0183
HilthEXE <endranomarklewence@gmail.com>
2023-03-21 21:31:52 +0800

    Finished configuring Laravel Websockets, Laravel Echo, and pusher

---
+++ app/Events/TestEvent.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TestEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new Channel('test-channel');
+    }
+
+    // Custome Name of the Event
+    public function broadcastAs()
+    {
+        return 'test-event';
+    }
+
+    // Data to be passed to the event
+    public function broadcastWith()
+    {
+        return [
+            'test' => 'I am working',
+        ];
+    }
+}
