85ab9a247c2b97a6e8b39c0ed4a5734ac9a6b5c2
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-03-21 23:23:54 +0800

    fix errors

--- resources/views/components/signed-clearance.blade.php
+++ resources/views/components/signed-clearance.blade.php
@@ -1,5 +1,6 @@
 <div class="card bg-white-50 rounded-lg shadow w-100 px-4 py-3 mb-2"
-    style="border-left: 8px solid #17A2B8; background-color: #f7f7f7;">
+    style="border-left: 5px solid {{ $clearancestatus === 'Cleared' ? '#a7d2ad' : '#ff0000' }}; background-color:
+    #f7f7f7;">
     <div class="d-flex justify-content-between align-items-center">
         <div class="d-flex align-items-center">
             <button class="btn btn-link text-kyoodark" type="button" id="queueInfoToggle" data-bs-toggle="dropdown"
