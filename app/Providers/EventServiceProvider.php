1251f111a8f87ac17677e968f0e07b8861403a9b
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-03-23 23:03:57 +0800

    Revert "Livequeue websockets"

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Events\LiveQueueEvent;
-use App\Listeners\LiveQueue;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -20,11 +18,6 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        LiveQueueEvent::class => [
-            LiveQueue::class,
-        ],
-        // Event::class => [Listener::class],
-        // ! NOTE: Execute php artisan event:generate after registering the event listener
     ];
 
     /**
--- resources/views/components/current-serving-card.blade.php
+++ resources/views/components/current-serving-card.blade.php
@@ -4,11 +4,11 @@
             <h5 class="card-title fw-bold" style="font-size: clamp(1rem, 2vw, 1.5rem);">
                 {{ $department->name }}</h5>
             @if ($department->status == 'active')
-                <div class="d-flex justify-content-center align-items-center" id="display-ticket-{{ $department->id }}">
-                    {{-- @if ($department->ticket_number)
-                        <div class="d-flex flex-column align-items-center serving-ticket">
+                <div class="d-flex justify-content-center align-items-center">
+                    @if ($department->ticket_number)
+                        <div class="d-flex flex-column align-items-center">
                             <h1 class="card-subtitle mb-2" style="font-size: clamp(2rem, 5vw, 3rem);">
-                            </h1>
+                                {{ $department->ticket_number }}</h1>
                             <span class="text-primary fw-semibold"
                                 style="font-size: clamp(0.8rem, 2vw, 1.2rem);">Currently
                                 Serving</span>
@@ -20,14 +20,7 @@
                                 style="font-size: clamp(0.8rem, 2vw, 1.2rem);">Currently Being
                                 Served</span>
                         </div>
-                    @endif --}}
-
-                    <div class="d-flex flex-column align-items-center">
-                        <h1 class="card-subtitle mb-2" style="font-size: clamp(2rem, 5vw, 3rem);">No ticket</h1>
-                        <span class="text-warning fw-semibold" style="font-size: clamp(0.8rem, 2vw, 1.2rem);">Currently
-                            Being
-                            Served</span>
-                    </div>
+                    @endif
                 </div>
             @else
                 <div class="d-flex flex-column align-items-center">
