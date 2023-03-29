07e5d15db705436d7fed90da6733b3096a4bca16
Ronald Vergel Dela Cruz <77273407+rnldvrgl@users.noreply.github.com>
2023-03-29 19:15:54 +0800

    modify error 403 page text

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -10,8 +10,7 @@
                     <div class="col-md-7">
                         <img src="{{ asset('assets/images/error-403.png') }}" class="img-fluid py-5" alt="Page Not Found">
                     </div>
-                    <div
-                        class="col-md-5 d-flex flex-column justify-content-center align-items-right text-center text-md-left gap-3">
+                    <div class="col-md-5 d-flex flex-column justify-content-center text-center text-md-start gap-3">
                         <h1>Access Denied</h1>
                         <h2>Sorry, you do not have permission to access this page.</h2>
                         <p>If you believe this is an error, please contact the website administrator.</p>
